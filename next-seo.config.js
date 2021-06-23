372ce04170c1cd00baee5cf1f44149bb8e876d92
Ertan Kayalar <ertankayalar@gmail.com>
2021-06-23 11:25:27 +0300

    next seo plugin

---
+++ next-seo.config.js
@@ -0,0 +1,12 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    site_name: 'Under Microscope',
+  },
+  twitter: {
+    handle: '@handle',
+    site: '@site',
+    cardType: 'summary_large_image',
+  },
+}
