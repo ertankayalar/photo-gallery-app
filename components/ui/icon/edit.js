e6eb2c7f4049dd78a47bb98ef90a5840c432b401
Ertan Kayalar <ertankayalar@gmail.com>
2021-06-23 11:24:24 +0300

    icon syntax fix

--- components/ui/icon/edit.js
+++ components/ui/icon/edit.js
@@ -6,7 +6,7 @@ const EditIcon = ({ className = 'h-6 w-6' }) => {
       xmlns='http://www.w3.org/2000/svg'
       className={className}
       viewBox='0 0 24 24'
-      stroke-width='1.5'
+      strokeWidth='1.5'
       // stroke='#2c3e50'
       stroke='currentColor'
       fill='none'
--- components/ui/icon/upload.js
+++ components/ui/icon/upload.js
@@ -6,7 +6,7 @@ const UploadIcon = ({ className }) => {
       xmlns='http://www.w3.org/2000/svg'
       className={className}
       viewBox='0 0 24 24'
-      stroke-width='1.5'
+      strokeWidth='1.5'
       stroke='currentColor'
       fill='none'
       stroke-linecap='round'
