2079b24808f4801f299d4e5f0faf47035500463f
Ertan Kayalar <ertankayalar@gmail.com>
2021-06-30 18:31:16 +0300

    blog post

---
+++ components/post/post-header.js
@@ -0,0 +1,42 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import DateFormatter from '../ui/date-formater'
+
+export default function PostHeader({ post }) {
+  const { title, coverimage, published_at, blog_categories } = post
+
+  const coverImageUrl = coverimage?.formats?.large?.url
+  const coverImageWidth = coverimage?.formats?.large?.width
+  const coverImageHeight = coverimage?.formats?.large?.height
+  return (
+    <header className='w-full md:max-w-3xl mx-auto'>
+      <h1 className='text-3xl mb-3 md:text-4xl my-10 text-center text-gray-700 font-header'>
+        {title}
+      </h1>
+
+      <div className='text-gray-500 my-5 text-xs md:text-sm flex justify-center items-center'>
+        <span className='mr-5 pl-5' itemProp='datePublished'>
+          <DateFormatter dateString={published_at} />
+        </span>
+        <span className=' border-l pl-5 text-xs md:text-md font-semibold'>
+          {blog_categories.map((cat) => (
+            <Link href={`/blo/category/${cat.slug}/`} key={cat.slug}>
+              <a className='mr-3 hover:underline'>{cat.name}</a>
+            </Link>
+          ))}
+        </span>
+      </div>
+
+      <div className='w-full md:max-w-2xl mx-auto'>
+        {coverImageUrl && (
+          <Image
+            src={coverImageUrl}
+            alt={title}
+            width={coverImageWidth}
+            height={coverImageHeight}
+          />
+        )}
+      </div>
+    </header>
+  )
+}
---
+++ pages/blog/[slug].js
@@ -0,0 +1,99 @@
+import React from 'react'
+import axios from 'axios'
+import remark from 'remark'
+import html from 'remark-html'
+import Container from '../../components/layout/container'
+import Layout from '../../components/layout/layout'
+import PageHeader from '../../components/layout/page-header'
+import PostHeader from '../../components/post/post-header'
+import Breadcrumb from '../../components/ui/breadcrumb'
+
+function Post({ post, contentHtml }) {
+  console.log(`post`, post)
+  console.log(`contentHtml`, contentHtml)
+  const { title, excerpt, content, blog_categories } = post
+
+  const breadcrumbs = [
+    { url: '/', name: 'Home' },
+    {
+      url: `/blog`,
+      name: `Blog`,
+      last: true,
+    },
+  ]
+
+  return (
+    <Layout>
+      <Container>
+        <Breadcrumb breadcrumbs={breadcrumbs} />
+      </Container>
+      <Container>
+        <PostHeader post={post} />
+      </Container>
+
+      <Container>
+        <div
+          className='prose prose-sm md:prose-md w-full md:max-w-3xl mx-auto my-5'
+          dangerouslySetInnerHTML={{ __html: contentHtml }}
+        ></div>
+      </Container>
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  const result = await axios.get(`${process.env.API_URL}/blog-posts`)
+  if (result.error) {
+    console.log('Error:', result.error)
+  }
+
+  console.log(`result.data`, result.data)
+
+  const paths = result.data.map((post) => {
+    return {
+      params: {
+        slug: post.slug,
+      },
+    }
+  })
+  // const { API_URL } = process.env
+  // const res = await fetch(`${API_URL}/pages`)
+  // const data = await res.json()
+  // const paths = data.map((page) => {
+  //   return {
+  //     params: {
+  //       slug: page.slug,
+  //     },
+  //   }
+  // })
+
+  console.log(`paths`, paths)
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const { slug } = params
+
+  const result = await axios.get(
+    `${process.env.API_URL}/blog-posts?slug=${slug}`
+  )
+
+  if (result.error) {
+    console.log('Error:', result.error)
+  }
+
+  const postData = result.data
+
+  const processedContent = await remark().use(html).process(postData[0].content)
+  const contentHtml = processedContent.toString()
+
+  console.log(`postData`, postData[0])
+  return {
+    props: { post: postData[0], contentHtml },
+  }
+}
+
+export default Post
